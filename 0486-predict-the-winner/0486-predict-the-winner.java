5dd8ab487169b1a159d43accf40c3c5957437e45
NITHYA SRI D <nithyasridevasenan9706@gmail.com>
2026-08-01 21:10:16 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.44%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        // Base case: when there is only one number
+        for (int i = 0; i < n; i++) {
+            dp[i] = nums[i];
+        }
+
+        // Build DP from smaller subarrays to larger
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[j] = Math.max(nums[i] - dp[j],
+                                 nums[j] - dp[j - 1]);
+            }
+        }
+
+        return dp[n - 1] >= 0;
+    }
+}

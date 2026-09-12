e7f91b8e91d7951616db0df41482a2d66af17c61
NITHYA SRI D <nithyasridevasenan9706@gmail.com>
2026-09-12 16:03:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,23 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int rows = matrix.length;
+        int rows =matrix.length;
         int col = matrix[0].length;
-        boolean[] zerorows = new boolean[rows];
-        boolean[] zerocol = new boolean[col];
-
+        boolean[] zrow = new boolean[rows];
+        boolean[] zcol = new boolean[col];
         for(int i =0;i<rows;i++){
             for(int j =0;j<col;j++){
                 if(matrix[i][j] == 0){
-                    zerorows[i] = true;
-                    zerocol[j] = true;
+                    zrow[i] = true;
+                    zcol[j] = true;
                 }
             }
         }
-        for(int i=0 ;i<rows;i++){
-            for(int j=0;j<col;j++){
-                if(zerorows[i] || zerocol[j]){
+        for(int i=0;i<rows;i++){
+            for(int j =0;j<col;j++){
+                if(zrow[i]||zcol[j]){
                     matrix[i][j] = 0;
-                }            
+
+                }
+
             }
         }
     }
